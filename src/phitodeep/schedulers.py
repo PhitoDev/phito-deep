26357624a9f6f18d6b6c60d8c054fcf1f0f7da8b
Ralph <rdugue1@gmail.com>
2026-05-01 20:45:38 -0400

    Added new activation functions
    - LeakyReLU
    - GELU
    - Swish

--- src/phitodeep/schedulers.py
+++ src/phitodeep/schedulers.py
@@ -2,5 +2,17 @@ import numpy as np
 
 
 class AlphaScheduler:
-    def __ini__(self, alpha):
+    def __ini__(self, name: str, alpha: np.float16) -> None:
+        self.name = name
         self.alpha = alpha
+
+    def step(self, time_step: int) -> np.float16:
+        raise NotImplementedError(f"{self.name} must implement the step function")
+
+class InverseTimeDecay(AlphaScheduler):
+    def __init__(self, alpha: np.float16, decay_rate: np.float16):
+        super().__init__("InverseTimeDecay", alpha)
+        self.decay_rate = decay_rate
+
+    def step(self, time_step: int) -> np.float16:
+        return self.alpha / (1 + self.decay_rate * time_step)
